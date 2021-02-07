9b842223ab9d0b9516d0452f45e01ecb29d43802
George Burduli <burduli94@gmail.com>
2021-02-07 13:43:20 +0400

    Removed unnecessary WP and WC styles

--- functions.php
+++ functions.php
@@ -20,9 +20,27 @@ function mytheme_add_woocommerce_support() {
 
 add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 
-// Disable WooCommerce Styles
+// Disable Gutenberg
+add_filter('use_block_editor_for_post_type', '__return_false', 10);
 
-// add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+// Disable Gutenberg styles
+function remove_woo_blocks(){
+  wp_dequeue_style( 'wp-block-library' );
+  wp_dequeue_style( 'wp-block-library-theme' );
+  wp_dequeue_style( 'wc-block-style' );
+}
+
+add_action( 'wp_enqueue_scripts', 'remove_woo_blocks', 100 );
+
+// Disable WooCommerce styles
+add_filter( 'woocommerce_enqueue_styles', 'jk_dequeue_styles' );
+function jk_dequeue_styles( $enqueue_styles ) {
+	// unset( $enqueue_styles['woocommerce-general'] );	// Remove the gloss
+	unset( $enqueue_styles['woocommerce-layout'] );		// Remove the layout
+  unset( $enqueue_styles['woocommerce-smallscreen'] );	// Remove the smallscreen optimisation
+  
+	return $enqueue_styles;
+}
 
 // Includes
 
