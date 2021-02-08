e83408a272827529a22fb86b138a9f877407596b
George Burduli <burduli94@gmail.com>
2021-02-08 13:51:58 +0400

    Added service backgrounds on desktop

--- services.php
+++ services.php
@@ -9,25 +9,25 @@
         <div class="service__logo logo__yellow">
           <i class="uil uil-shield-check"></i>
         </div>
-        <p class="service__title">გარანტია თითოეულ პროდუქტზე</p>
+        <p class="service__title">გარანტია თითოეულ<wbr /> პროდუქტზე</p>
       </div>
       <div class="service__item">
         <div class="service__logo logo__red">
           <i class="uil uil-box"></i>
         </div>
-        <p class="service__title">პროდუქტის დაბრუნების პოლიტიკა</p>
+        <p class="service__title">პროდუქტის<br /> დაბრუნების პოლიტიკა</p>
       </div>
       <div class="service__item">
         <div class="service__logo logo__blue">
           <i class="uil uil-laptop-connection"></i>
         </div>
-        <p class="service__title">ონლაინ გადახდა ერთ წუთში</p>
+        <p class="service__title">ონლაინ გადახდა<br /> ერთ წუთში</p>
       </div>
       <div class="service__item">
         <div class="service__logo logo__purple">
           <i class="uil uil-globe"></i>
         </div>
-        <p class="service__title">მიტანის სერვისი</p>
+        <p class="service__title">მიტანის<br /> სერვისი</p>
       </div>
     </div>
   </div>
